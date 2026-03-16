c6630e90e24e5706a5e6c8e0547e4decd0141efb
Vlad1slav1k <wukelanvlad@gmail.com>
2026-03-16 21:13:49 +0200

    Update jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -2,59 +2,71 @@ pipeline {
     agent any
 
     parameters {
-        choice(name: 'OS', choices: ['linux','apple','windows'], description: 'Pick OS')
-        choice(name: 'ARCH', choices: ['amd64','arm64'], description: 'Pick ARCH')
+        choice(name: 'OS', choices: ['linux', 'apple', 'windows'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
     }
 
     environment {
         GITHUB_TOKEN = credentials('jenkins')
         REPO = 'https://github.com/Vlad1slav1k/kbot.git'
         BRANCH = 'develop'
+        REGISTRY = 'ghcr.io/vlad1slav1k'
     }
 
     stages {
 
-        stage('clone') {
+        stage('Clone Repository') {
             steps {
                 git branch: "${BRANCH}", url: "${REPO}"
             }
         }
 
-        stage('test') {
+        stage('Test') {
             steps {
-                sh 'make test'
+                echo "Running tests..."
+                sh "make TARGETOS=${params.OS} TARGETARCH=${params.ARCH} test"
             }
         }
 
-       stage('build') {
-          steps {
-              echo "Building binary for platform ${params.OS} on ${params.ARCH} started"
-              sh "make build-${params.OS}-${params.ARCH}"
+        stage('Build') {
+            steps {
+                echo "Building binary for ${params.OS}-${params.ARCH}"
+                sh "make TARGETOS=${params.OS} TARGETARCH=${params.ARCH} build"
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                echo "Building Docker image for ${params.OS}-${params.ARCH}"
+                sh "make TARGETOS=${params.OS} TARGETARCH=${params.ARCH} image"
+            }
+        }
+
+        stage('Login to GHCR') {
+            steps {
+                withCredentials([
+                    usernamePassword(
+                        credentialsId: 'github-packages',
+                        usernameVariable: 'GITHUB_USER',
+                        passwordVariable: 'GITHUB_TOKEN_PSW'
+                    )
+                ]) {
+                    sh 'echo $GITHUB_TOKEN_PSW | docker login ghcr.io -u $GITHUB_USER --password-stdin'
+                }
+            }
+        }
+
+        stage('Push Docker Image') {
+            steps {
+                sh "make TARGETOS=${params.OS} TARGETARCH=${params.ARCH} push"
+            }
+        }
     }
-}
 
-        stage('image') {
-            steps {
-               sh "make ${params.OS} ${params.ARCH}"
-            }
-        }
-
-        stage('login GHCR') {
-            steps {
-                sh "echo $GITHUB_TOKEN_PSW | docker login ghcr.io -u $GITHUB_TOKEN_USR --password-stdin"
-            }
-        }
-
-        stage('push image') {
-            steps {
-                sh "make ${params.OS} ${params.ARCH} image push"
-            }
-        }
-
-        stage('logout') {
-            steps {
-                sh 'docker logout || true'
-            }
+    post {
+        always {
+            echo 'Logging out from Docker...'
+            sh 'docker logout'
         }
     }
 }
